ce7443ed24d55ac4210158242d58fe1fa718ea56
@960bg Mihail Dementev <-h>
2022-08-13 02:59:57 +0400

    feat: done task 50%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,148 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, ...bracketsConfig) {
   // your solution
+  // стэк для определения порядка следования скобок
+  let stack = [];
+
+  // спец стек для определия последовательности каогда откр и закр скобки одинаковы как ||
+  let spec_stack = []
+  // словарь для видов скобок
+  let bracket_pair = {};
+
+  // массив видов открывающих скобок
+  let open_bracket = [];
+  // массив видов закрывающих скобок чтобы можно было определить когда откр и закр скобки одинаковы как ||
+  let close_bracket = [];
+
+  for (i = 0; i < bracketsConfig.length; i++) {
+    for (j = 0; j < bracketsConfig[i].length; j++) {
+      // заполняем словарь видов скобок
+      bracket_pair[`${bracketsConfig[i][j][1]}`] = `${bracketsConfig[i][j][0]}`;
+      // заполняем массив видов открывающих скобок
+      open_bracket.push(bracketsConfig[i][j][0]);
+      close_bracket.push(bracketsConfig[i][j][1]);
+
+      // console.log('===========');
+      // console.log(bracket_pair);
+      // console.log('===========');
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    // текущий элемент 
+    let curr_el = str[i];
+
+    // текущий элемент - открывающая скобка
+    if (open_bracket.includes(curr_el)) {
+      // а также если текущий элемент - закрывающая  скобка т.е. ||
+      // if (close_bracket.includes(curr_el) && (stack.length === 0)) {
+      //   spec_stack.push(curr_el);
+      // }
+
+
+
+      stack.push(curr_el);
+
+      if ((close_bracket.includes(curr_el)) && (spec_stack.length % 2 == 0)) {
+        spec_stack.push(curr_el);
+      }
+      //   if ((close_bracket.includes(str[i + 1]) && (!open_bracket.includes(str[i + 1])) && (i !== (str.length - 1)))) {
+      //     console.log('\n стрsdsdsdока: ' + str);
+      //     console.log(false);
+      //     return false;
+
+      //   } else {
+      //     spec_stack.push(curr_el);
+      //   }
+      // }
+
+
+    } else {
+      // если текущий эл не открыв скобка и стэк пуст значит ошибка
+      if (stack.length === 0) {
+        console.log('\n строка: ' + str);
+        console.log(false);
+        return false;
+      }
+
+
+      // последний элемент в стэке
+      let top_el_stack = stack[stack.length - 1];
+      // предыдущий элемент в стэке
+      // let prev_el_stack = stack[stack.length - 2];
+      // следующий элемент в строке!
+      // let next_el_str = str[i + 1]; stack[stack.length - 1];
+
+
+
+
+      // если элемент не открыв скобка и стэк не пуст значит найти парную скобку
+      // используем словарь пар для скобок и последний элемент в стэке сравниваем
+      if (bracket_pair[curr_el] === top_el_stack) {
+        // если наш тек эл == посл эл в стэке - значит пара совпала скобка закрыта
+        // нужно удалить этот последний элемент из стека
+
+        // if (close_bracket.includes(curr_el) && open_bracket.includes(curr_el) &&
+        //   (spec_stack.length > 1) && (i !== (str.length - 1)) && (str[i + 1] !== curr_el)) {
+        //   console.log('\n sddsdsdsds   строка: ' + str);
+        //   console.log(false);
+        //   return false;
+        // }
+
+        stack.pop();
+
+      } else {
+
+        console.log('\n строка: ' + str);
+        console.log(false);
+        return false;
+      }
+
+      // if (close_bracket.includes(curr_el) && open_bracket.includes(curr_el)) {
+      //   spec_stack.pop();
+      // }
+      // последний элемент в спец стэке
+      let top_spec_stack = spec_stack[spec_stack.length - 1];
+      // если скобки одинаковы ||
+      // if (close_bracket.includes(curr_el) && (open_bracket.includes(curr_el)) && (top_spec_stack === bracket_pair[curr_el])) {
+      //   spec_stack.pop();
+      // }
+
+
+
+
+
+    }
+  }
+
+
+
+
+  console.log('\n строка: ' + str);
+  console.log('\n словарь для видов скобок: ');
+  console.log(bracket_pair);
+  console.log('=---------------=');
+  console.log('массив видов открывающих скобок: ');
+  console.log(open_bracket);
+  console.log('=---------------=');
+  console.log('spec_stack.length: ' + spec_stack.length);
+
+  console.log(((stack.length === 0) && (spec_stack.length === 0)));
+  return ((stack.length === 0) && (spec_stack.length === 0));
 }
+
+
+
+// check('()', [['(', ')']]); // -> true
+// check('((()))()', [['(', ')']]); // -> true
+// check('())(', [['(', ')']]); // -> false
+// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]); // -> true
+// check('[(])', [['(', ')'], ['[', ']']]); // -> false
+// check('[]()', [['(', ')'], ['[', ']']]); // -> true
+// check('[]()(', [['(', ')'], ['[', ']']]); // -> false
+
+// // special case: opening and closing bracket can be the same :)
+
+// check('||', [['|', '|']]); // -> true
+// check('|()|', [['(', ')'], ['|', '|']]); // -> true
+// check('|(|)', [['(', ')'], ['|', '|']]); // -> false
+// check('|()|(||)||', [['(', ')'], ['|', '|']]); // -> true
